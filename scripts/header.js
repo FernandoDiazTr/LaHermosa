866efa578b17d1453e16bbca7aeab61af4f48228
dlopel <d.lopeloyola@gmail.com>
2021-12-10 02:43:05 -0500

    desktop header sticky

--- scripts/header.js
+++ scripts/header.js
@@ -3,20 +3,26 @@ const logoImgList = Array.from(document.querySelectorAll('.header .header__logo-
 logoImgList[1].style.display = 'none'
 
 //funcionalidad para manipular el menu movil lateral
-let isMobileMenuOpen = false
 const barsButton = document.querySelector('.header .header__mobile-bars')
+const mobileMenu = document.querySelector('.header .mobile-menu')
 barsButton.addEventListener('click', e => {
-    const mobileMenu = document.querySelector('.header .mobile-menu')
-    
-    if (!isMobileMenuOpen) {
-        e.target.classList.remove('fa-bars')
-        e.target.classList.add('fa-times')
-        mobileMenu.classList.add('mobile-menu_open')
-        isMobileMenuOpen = true
-    } else {
-        e.target.classList.remove('fa-times')
-        e.target.classList.add('fa-bars')
-        mobileMenu.classList.remove('mobile-menu_open')
-        isMobileMenuOpen = false
-    }
+    e.target.classList.toggle('fa-bars')
+    e.target.classList.toggle('fa-times')
+    mobileMenu.classList.toggle('mobile-menu_open')
 })
+
+//cuando el header se queda pegado
+const observer = new IntersectionObserver(
+    ([e]) => {
+        e.target.toggleAttribute('stuck', e.intersectionRatio < 1)
+        logoImgList[0].style.display = 'none'
+        logoImgList[1].style.display = 'block'
+
+        if (e.intersectionRatio === 1) {
+            logoImgList[0].style.display = 'block'
+            logoImgList[1].style.display = 'none'
+        }
+    },
+    { threshold: 1.0 }
+)
+observer.observe(document.querySelector('header.header'))
